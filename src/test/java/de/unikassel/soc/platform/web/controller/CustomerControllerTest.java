365f47f96d7b85570b621359d97729e96b11ce1d
kai <kai.hardenbicker@gmail.com>
2021-01-12 14:49:07 +0100

    Implemented Tests for CustomerController. Closes #7

--- src/test/java/de/unikassel/soc/platform/web/controller/CustomerControllerTest.java
+++ src/test/java/de/unikassel/soc/platform/web/controller/CustomerControllerTest.java
@@ -1,15 +1,19 @@
 package de.unikassel.soc.platform.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.unikassel.soc.platform.services.CustomerService;
 import de.unikassel.soc.platform.web.model.CustomerDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
@@ -50,5 +54,43 @@ class CustomerControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/customer/" + uuid))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getCustomersByName() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+        String name = "Test 1";
+        List<CustomerDto> customerDtos = new ArrayList<>();
+        CustomerDto customerDto1 = new CustomerDto(UUID.randomUUID(), name, null);
+        customerDtos.add(customerDto1);
+
+        when(customerService.getCustomersByName(name)).thenReturn(customerDtos);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/customer/?name="+name ))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void handlePost() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+        CustomerDto customerDto = new CustomerDto(UUID.randomUUID(), "Test", null);
+        when(customerService.saveNewCustomer(customerDto)).thenReturn(customerDto);
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .post("/api/v1/customer")
+                        .content(asJsonString(customerDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
